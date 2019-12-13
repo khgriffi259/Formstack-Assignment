80274ab308d4a34e4fd150b2c1394225271e345e
Kyle Griffin <kyle.khgriffi@gmail.com>
2019-12-13 15:37:27 -0500

    Basic config of testing and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => 
+     <nav className="nav-extended">
+        <div className="nav-wrapper black">
+            <div className="container">
+                <div className="title center">Formstack Assignment</div>
+            </div>
+        </div>
+    </nav>
+
+export default Navbar
