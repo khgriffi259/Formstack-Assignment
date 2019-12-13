3076d86bdfc3397e700c8953166085fe07b239f0
Kyle Griffin <kyle.khgriffi@gmail.com>
2019-12-13 15:58:14 -0500

    added react-router and setup home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'materialize-css/dist/css/materialize.css'
-import Navbar from './components/Navbar';
+
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home';
 
 const App = () => 
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />1
+    </Switch>
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,10 @@
-rafce
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => 
+    <div className="Home">
+    <Navbar />
+    this is home page
+    </div>
+
+export default Home
